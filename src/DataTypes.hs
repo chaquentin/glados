4694ba6fb090829b49c55d9425919d8acb72e544
AngeD <ange.duhayon@epitech.eu>
2023-09-18 16:59:16 +0200

    fix formatting

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -19,19 +19,19 @@ instance Show Expression where
   show = showExpression
 
 showExpression :: Expression -> String
-showExpression Null = "null"
-showExpression (Number n) = show n
-showExpression (Boolean True) = "#t"
-showExpression (Boolean False) = "#f"
-showExpression (Variable v) = v
-showExpression (Pair e1 e2) = "(" ++ show e1 ++ " . " ++ show e2 ++ ")"
-showExpression (Exception s) = "Exception: " ++ s
-showExpression (Lambda args body) = "(lambda (" ++ showArgs args ++ ") " ++ show body ++ ")"
+showExpression Null                   = "null"
+showExpression (Number n)             = show n
+showExpression (Boolean True)         = "#t"
+showExpression (Boolean False)        = "#f"
+showExpression (Variable v)           = v
+showExpression (Pair e1 e2)           = "(" ++ show e1 ++ " . " ++ show e2 ++ ")"
+showExpression (Exception s)          = "Exception: " ++ s
+showExpression (Lambda args body)     = "(lambda (" ++ showArgs args ++ ") " ++ show body ++ ")"
 showExpression (PrimitiveProcedure _) = "<primitive>"
-showExpression (Application e es) = "(" ++ show e ++ " " ++ showArgs es ++ ")"
-showExpression (Definition e1 e2) = "(define " ++ show e1 ++ " " ++ show e2 ++ ")"
-showExpression (If e1 e2 e3) = "(if " ++ show e1 ++ " " ++ show e2 ++ " " ++ show e3 ++ ")"
-showExpression (Cond clauses) = "(cond " ++ showClauses clauses ++ ")"
+showExpression (Application e es)     = "(" ++ show e ++ " " ++ showArgs es ++ ")"
+showExpression (Definition e1 e2)     = "(define " ++ show e1 ++ " " ++ show e2 ++ ")"
+showExpression (If e1 e2 e3)          = "(if " ++ show e1 ++ " " ++ show e2 ++ " " ++ show e3 ++ ")"
+showExpression (Cond clauses)         = "(cond " ++ showClauses clauses ++ ")"
 
 showArgs :: [Expression] -> String
 showArgs = unwords . map show
