ef25cdc834f1bfd90fb1741847962ca56c90ad77
Benjamin <benjamin.bourez@epitech.eu>
2023-10-03 11:34:33 +0200

    REFACTO: gestion args and file

--- src/ReadFile.hs
+++ src/ReadFile.hs
@@ -7,15 +7,24 @@
 
 module ReadFile (readFileIfExists) where
 import System.IO
+import System.Directory
 
 readFileIfExists :: String -> IO ()
 readFileIfExists filename = do
-    fileContents <- readFileSafe filename
-    case fileContents of
-        Just contents -> do
-            putStrLn "file content :"
-            putStrLn contents
-        Nothing -> putStrLn $ "file " ++ filename ++ " does not exist."
+    fileExists <- doesFileExist filename
+    if fileExists
+        then do
+            filePermissions <- getPermissions filename
+            if readable filePermissions
+                then do
+                    fileContents <- readFileSafe filename
+                    case fileContents of
+                        Just contents -> do
+                            putStrLn "File contents :"
+                            putStrLn contents
+                        Nothing -> putStrLn $ "File " ++ filename ++ " is empty."
+                else putStrLn $ "You don't have read permissions for file " ++ filename ++ "."
+        else putStrLn $ "File " ++ filename ++ " does not exist."
 
 readFileSafe :: String -> IO (Maybe String)
 readFileSafe filename = do
