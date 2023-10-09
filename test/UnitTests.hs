a5f3e2dcfdbd4169b75c85c25250bce04810a7e1
Quentin <quentin.challon@epitech.eu>
2023-10-09 14:21:41 +0200

    Add temp tests

--- test/UnitTests.hs
+++ test/UnitTests.hs
@@ -1,7 +1,9 @@
 import Test.HUnit
 import qualified System.Exit as Exit
+import System.IO
 import Parser (parseAst)
 import DataTypes (Ast (..))
+import Execute (printExecute)
 
 test1 :: Test
 test1 = TestCase (assertEqual "should return Number 3" (parseAst "3") (Number 3))
@@ -15,12 +17,18 @@ test3 = TestCase (assertEqual "should return List(Number 1, Number 2)" (parseAst
 test4 :: Test
 test4 = TestCase (assertEqual "should return List [Define 'ok' (Number 3)]" (parseAst ("(define ok 3)")) (List [Define "ok" (Number 3)]))
 
+test5 :: Test
+test5 = TestCase $ do
+    (output, _) <- capture_ $ printExecute (parseAst "(+ 2 3)")
+    assertEqual "should return Number 5" (output) (Number 5)
+
 tests :: Test
 tests = TestList
     [ TestLabel "Basic Parser test with number" test1
     , TestLabel "Basic Parser test with variable" test2
     , TestLabel "Basic Parser test with bool" test3
     , TestLabel "Parsing define variable" test4
+    , TestLabel "Basic execute test" test5
     ]
 
 main :: IO ()
