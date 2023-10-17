e9d3dbc3d605a09e57d3dd17908a295440c83b39
Benjamin <benjamin.bourez@epitech.eu>
2023-10-17 11:02:10 +0200

    ADD: bootstrap vm

--- src/VM.hs
+++ src/VM.hs
@@ -3,7 +3,21 @@ module VM where
 data Value
     = Number Int
     | Boolean Bool
-    deriving (Show, Eq)
+    | Builtin Builtin
+    | Function String Program 
+
+instance Show Value where
+    show (Number n) = show n
+    show (Boolean b) = show b
+    show (Builtin n) = "<built-in function>" ++ show n
+    show (Function n p) = "<function>" ++ n
+
+instance Eq Value where
+    (Number n) == (Number n') = n == n'
+    (Boolean b) == (Boolean b') = b == b'
+    (Builtin _) == (Builtin _) = True
+    (Function n _) == (Function n' _) = n == n'
+    _ == _ = False
 
 data Builtin
     = Add
@@ -12,15 +26,20 @@ data Builtin
     | Div
     | Eqq
     | Less
+    deriving (Show, Eq)
 
 data Instruction
     = Push Value
-    | Call Builtin
+    | Call
     | JumpIfFalse Int
+    | PushArg Int
+    | PushEnv String
     | Ret
 
 type Stack = [Value]
 type Program = [Instruction]
+type Args = [Value]
+type Env = [(String, Program)]
 
 add :: Stack -> Either String Stack
 add (Number x:Number y:xs) = Right $ Number (x + y) : xs
@@ -48,26 +67,37 @@ jumpIfFalse n (Boolean False:_) p = Right (drop n p)
 jumpIfFalse _ (Boolean True:_) p = Right p
 jumpIfFalse _ _ _= Left "Invalid arguments for jumpIfFalse"
 
-call :: Builtin -> Stack -> Either String Stack
-call Add = add
-call Sub = sub
-call Mul = mul
-call Div = divi
-call Eqq = eqq
-call Less = less
+call :: Env -> Value -> Stack -> Either String Stack
+call _ (Builtin Add) s = add s
+call _ (Builtin Sub) s = sub s
+call _ (Builtin Mul) s = mul s
+call _ (Builtin Div) s = divi s
+call _ (Builtin Eqq) s = eqq s
+call _ (Builtin Less) s = less s
+call env (Function _ p) (s:xs) = case exec [s] env p [] of
+    Left err -> Left err
+    Right (Number n) -> Right $ Number n : xs
+    Right (Boolean b) -> Right $ Boolean b : xs
+    Right (Function n p) -> Right $ Function n p : xs
+    Right (Builtin b) -> Right $ Builtin b : xs
+call _ _ _ = Left "Invalid call"
 
 eqq :: Stack -> Either String Stack
 eqq (Number x:Number y:xs) = Right $ Boolean (x == y) : xs
 eqq (Boolean x:Boolean y:xs) = Right $ Boolean (x == y) : xs
 eqq _ = Left "Invalid arguments for eq?"
 
-exec :: Program -> Stack -> Either String Value
-exec ((Push v):xs) s = exec xs (v : s)
-exec ((Call x):xs) s = case call x s of
+exec :: Args -> Env -> Program -> Stack -> Either String Value
+exec a env ((Push v):xs) s = exec a env xs (v : s)
+exec a env ((PushArg n):xs) s = exec a env xs ((a !! n) : s)
+exec a env (Call:xs) s = case call env (head s) (tail s) of
     Left err -> Left err
-    Right s' -> exec xs s'
-exec ((JumpIfFalse n):xs) s = case jumpIfFalse n s xs of
+    Right p -> exec a env xs p
+exec a env ((JumpIfFalse n):xs) s = case jumpIfFalse n s xs of
     Left err -> Left err
-    Right p -> exec p s
-exec (Ret:_) (v:_) = Right v
-exec _ _ = error "Invalid progra"
+    Right p -> exec a env p s
+exec a env ((PushEnv n):xs) s = case lookup n env of
+    Nothing -> Left "Invalid env"
+    Just p -> exec a env xs (Function n p : s)
+exec a env (Ret:_) (v:_) = Right v
+exec _ _ _ _ = error "Invalid progra"
