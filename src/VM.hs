bc89177cd5dc7451d69e9f477d72f71d706e459e
Benjamin <benjamin.bourez@epitech.eu>
2023-10-18 16:20:01 +0200

    Add: arg for function

--- src/VM.hs
+++ src/VM.hs
@@ -4,19 +4,19 @@ data Value
     = Number Int
     | Boolean Bool
     | Builtin Builtin
-    | Function String Program 
+    | Function String Int Program 
 
 instance Show Value where
     show (Number n) = show n
     show (Boolean b) = show b
-    show (Builtin n) = "<built-in function>" ++ show n
-    show (Function n p) = "<function>" ++ n
+    show (Builtin n) = "<built-in function> " ++ show n
+    show (Function n a p) = "<function> " ++ n ++ " <nomber of args> " ++ show a
 
 instance Eq Value where
     (Number n) == (Number n') = n == n'
     (Boolean b) == (Boolean b') = b == b'
     (Builtin _) == (Builtin _) = True
-    (Function n _) == (Function n' _) = n == n'
+    (Function n _ _) == (Function n' _ _) = n == n'
     _ == _ = False
 
 data Builtin
@@ -39,7 +39,7 @@ data Instruction
 type Stack = [Value]
 type Program = [Instruction]
 type Args = [Value]
-type Env = [(String, Program)]
+type Env = [(String, (Int, Program))]
 
 add :: Stack -> Either String Stack
 add (Number x:Number y:xs) = Right $ Number (x + y) : xs
@@ -67,6 +67,10 @@ jumpIfFalse n (Boolean False:_) p = Right (drop n p)
 jumpIfFalse _ (Boolean True:_) p = Right p
 jumpIfFalse _ _ _= Left "Invalid arguments for jumpIfFalse"
 
+getargs :: Int -> Stack -> Args
+getargs 0 _ = []
+getargs n (x:xs) = x : (x:getargs (n - 1) xs)
+
 call :: Env -> Value -> Stack -> Either String Stack
 call _ (Builtin Add) s = add s
 call _ (Builtin Sub) s = sub s
@@ -74,11 +78,11 @@ call _ (Builtin Mul) s = mul s
 call _ (Builtin Div) s = divi s
 call _ (Builtin Eqq) s = eqq s
 call _ (Builtin Less) s = less s
-call env (Function _ p) (s:xs) = case exec [s] env p [] of
+call env (Function _ a p) (s:xs) = case exec (getargs a (s:xs)) env p [] of
     Left err -> Left err
     Right (Number n) -> Right $ Number n : xs
     Right (Boolean b) -> Right $ Boolean b : xs
-    Right (Function n p) -> Right $ Function n p : xs
+    Right (Function n ar p) -> Right $ Function n ar p : xs
     Right (Builtin b) -> Right $ Builtin b : xs
 call _ _ _ = Left "Invalid call"
 
@@ -98,6 +102,6 @@ exec a env ((JumpIfFalse n):xs) s = case jumpIfFalse n s xs of
     Right p -> exec a env p s
 exec a env ((PushEnv n):xs) s = case lookup n env of
     Nothing -> Left "Invalid env"
-    Just p -> exec a env xs (Function n p : s)
+    Just (ar, p) -> exec a env xs (Function n ar p : s)
 exec a env (Ret:_) (v:_) = Right v
 exec _ _ _ _ = error "Invalid progra"
