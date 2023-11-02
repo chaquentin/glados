726443c868fcf07d1ab71af5b60aa7194cabbd3a
Benjamin <benjamin.bourez@epitech.eu>
2023-11-02 14:57:55 +0100

    Add: new main for VM and compilo

--- app/Main.hs
+++ app/Main.hs
@@ -10,14 +10,22 @@ import System.Environment
 import ReadFile
 
 printHelp :: IO ()
-printHelp = putStrLn "Usage ./glados [FILENAME]"
-    >> putStrLn "\t [FILENAME]: file containing instructions to interpret"
+printHelp = putStrLn "Usage ./glados [--VM | --compile] [FILENAME]"
+    >> putStrLn "\t --VM: interpret the file"
+    >> putStrLn "\t --compile: compile the file to binary"
+    >> putStrLn "\t [FILENAME]: file containing instructions"
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
         ["-h"] -> printHelp
-        [filename] -> readFileIfExists filename
+        ["--VM", file] -> do
+            content <- readFileIfExists file
+            putStrLn content
+            putStrLn "a mettre ici la fonction de parse du binaire"
+        ["--compile", file] -> do
+            content <- readFileIfExists file
+            putStrLn content
+            putStrLn "a mettre ici la fonction de parse du fichier pour le compilo"
         _ -> putStrLn "Incorrect use. Use './glados -h' to display help."
-        
